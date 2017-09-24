cddd2e4320ecf18da1a0ae09c98662f25e970d0e
Fabian Beuke <mail@beuke.org>
2017-09-24 13:26:30 +0200

    bigQuery function

--- app/Main.hs
+++ app/Main.hs
@@ -7,18 +7,31 @@ import Data.Monoid (mconcat)
 import Network.HTTP.Conduit hiding (setRequestBodyLB)
 import Network.HTTP.Simple hiding (httpLbs)
 import Data.ByteString.Lazy.UTF8 (fromString)
+import Data.ByteString.Lazy (unpack)
+import Language.Haskell.TH.Ppr (bytesToString)
+import Data.String.Conversions (cs)
+
 import qualified Data.ByteString.Lazy as Lazy
 
-main = do
+bigQuery :: String -> IO Lazy.ByteString
+bigQuery query = do
   initReq <- parseUrl "https://pyapi-vida.herokuapp.com/bigquery"
-  let query = fromString "SELECT events.repo.name AS repo,\
-  \ COUNT(DISTINCT events.actor.id) AS stars\
-  \ FROM ( SELECT * FROM [githubarchive:day.20170920]) AS events\ 
-  \ WHERE events.type = 'WatchEvent'\
-  \ GROUP BY 1 ORDER BY 2 DESC LIMIT 1000"
-  let req = setRequestBodyLBS query $ initReq { secure = True
-                                              , method = "POST"
-                                              }
+  let req = setRequestBodyLBS (fromString query)
+                    $ initReq { secure = True
+                              , method = "POST"
+                              }
   res <- withManager $ httpLbs req
-  Lazy.putStr $ responseBody res
+  pure $ responseBody res
+
+main :: IO ()
+main = do
+  let query = "SELECT events.repo.name AS repo,\
+    \ COUNT(DISTINCT events.actor.id) AS stars\
+    \ FROM ( SELECT * FROM [githubarchive:day.20170920]) AS events\
+    \ WHERE events.type = 'WatchEvent'\
+    \ GROUP BY 1 ORDER BY 2 DESC LIMIT 1000"
+  result <- bigQuery query
+  let s = cs result :: String
+  scotty 3000 $
+    get "/:word" $ json s
 
